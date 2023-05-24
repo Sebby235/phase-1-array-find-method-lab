fd955773a6e8d98a63d4b5e6da50a8804d477161
Sebastian Martinez <isaacsoccer235@gmail.com>
2023-05-24 13:20:53 -0500

    finding array

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 // code your solution here
+function superbowlWin(collection) {
+    for (const record of collection) {
+        if (record.result === "W") {
+            console.log(record.year)
+            return record.year
+        }
+    }
+}
+const records = [
+    {year: 2015, result: "W"}
+]
+const winningYear =superbowlWin(records);
+console.log(winningYear)
